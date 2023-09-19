8ed065fbbcaf196bd19b5b260a83bb0951a465f5
Vyacheslav Tatarinov <tatarinov.slava222@gmail.com>
2023-09-19 21:41:15 +0300

    Lab_1 done

---
+++ WEB_153503_Tatarinov/WEB_153503_Tatarinov/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WEB_153503_Tatarinov.Models;
+
+namespace WEB_153503_Tatarinov.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
