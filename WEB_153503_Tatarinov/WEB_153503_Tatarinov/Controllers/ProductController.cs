7b4b846a2b5add0410756d8612f169f5c8997020
Vyacheslav Tatarinov <tatarinov.slava222@gmail.com>
2023-10-06 22:01:23 +0300

    3.4, 3.5, 3.6 done

---
+++ WEB_153503_Tatarinov/WEB_153503_Tatarinov/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using WEB_153503_Tatarinov.Services.CategoryService;
+using WEB_153503_Tatarinov.Services.ProductService;
+
+namespace WEB_153503_Tatarinov.Controllers;
+
+public class ProductController : Controller
+{
+    private IProductService _productService;
+    private ICategoryService _categoryService;
+
+    public ProductController(IProductService productService, ICategoryService categoryService)
+    {
+        _productService = productService;
+        _categoryService = categoryService;
+    }
+    // GET
+    public async Task<IActionResult> Index(string? category, int pageNo = 1)
+    {
+        var categoryResponse = await _categoryService.GetCategoryListAsync();
+        if(!categoryResponse.Success)
+            return NotFound(categoryResponse.ErrorMessage);
+        
+        ViewData["categories"] = categoryResponse.Data;
+        ViewData["selectedCategory"] = categoryResponse.Data?.SingleOrDefault(c => c.NormalizedName == category);
+        
+        var productResponce = await _productService.GetProductListAsync(category, pageNo);
+        if (!productResponce.Success)
+            return NotFound(productResponce.ErrorMessage);
+
+        return View(productResponce.Data);
+    }
+}
