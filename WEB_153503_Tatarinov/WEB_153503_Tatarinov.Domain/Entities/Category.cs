4bd75317a0f213fe53a3fa10576c2035773e8c1f
Vyacheslav Tatarinov <tatarinov.slava222@gmail.com>
2023-10-06 17:04:53 +0300

    Domain.Entities added (3.1 done)

---
+++ WEB_153503_Tatarinov/WEB_153503_Tatarinov.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+namespace WEB_153503_Tatarinov.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string NormalizedName { get; set; }
+}
---
+++ WEB_153503_Tatarinov/WEB_153503_Tatarinov.Domain/Entities/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEB_153503_Tatarinov.Domain.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    
+    [ForeignKey("Category")]
+    public int CategoryId { get; set; }
+    
+    public float Price { get; set; }
+    public string ImgPath { get; set; }
+    public string ImgMimeType { get; set; }
+    
+    public Category Category { get; set; }
+}
